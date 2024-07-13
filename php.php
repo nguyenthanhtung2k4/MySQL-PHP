42f9238612d9bd5fdb57add2f2f4eb7c29428b2f
JUSST <justsmoke2k4master@gmail.com>
2024-07-13 15:12:44 +0700

    thanhtung

--- php.php
+++ php.php
@@ -1,6 +1,20 @@
-<?php
-$a = 'tung';
-$b = 'tùng';
-echo(strlen('Độ dài chuỗi: '.$a)); 
-echo(strlen("\nĐộ dài chuỗi: ".$b)); 
+<?php 
+declare(strict_types=1);
+declare(strict_types=1);
+     function add($a,$b){
+          return $a+$b;
+     }
+
+     echo(add('3',4));   
+
+     <?php
+     declare(strict_types=1);
+     
+     function add(int $x, int $y) {
+       return $x + $y;
+     }
+     
+     echo add(5, 3); // Hiển thị 8
+     echo add("5", 3); // Lỗi: Argument 1 passed to add() must be of the type int, string given
+     
 ?>
---
+++ tk_ForEach_vs_For.php
@@ -0,0 +1,11 @@
+<?php
+// sử khác nhau  của  for each 
+$arr=['tung','bi','quoc']; 
+// for each
+foreach( $arr as $value){
+     echo(" \nin ra $value");
+}
+// for
+for($i=0;$i<3;$i++){
+     echo("\n in ra: $arr[$i]");
+}
