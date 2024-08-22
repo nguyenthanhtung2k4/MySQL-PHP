70e972ffa8de7827ea896c526827f5a1de587720
JUSST <justsmoke2k4master@gmail.com>
2024-08-22 22:57:57 +0700

    cokkie

--- PHP/tk_Cookies.php
+++ PHP/tk_Cookies.php
@@ -1,10 +1,12 @@
 <?php
 // Thiết lập cookie
                setcookie('username', 'JohnDoe', time() + 3600); // Cookie tồn tại trong 36000 giây= 1giờ
+               setcookie('pass', 'thanhtung', time() + 3600); // Cookie tồn tại trong 36000 giây= 1giờ
 
                // Truy cập cookie
                if(isset($_COOKIE['username'])) {
                echo "Username: " . $_COOKIE['username'];
+               echo "Username: " . $_COOKIE['pass'];
                }
 
 ?>
